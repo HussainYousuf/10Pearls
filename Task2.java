039c581ebb7c2915d13ea4ff152dfa27f94bca13
HussainYousuf <hussainroker@gmail.com>
2020-05-15 18:44:44 +0500

    add random cases

--- Task2.java
+++ Task2.java
@@ -8,35 +8,54 @@ public class Task2 {
         return Math.floorMod(a, 2) == 1;
     }
 
-    int[] Sort(int[] arr){
+    int[] Sort(int[] arr) {
         int temp[] = new int[arr.length];
         int k = 0;
         for (int i = 0; i < arr.length; i++) {
-            if(isOdd(arr[i])) temp[k++] = arr[i];
+            if (isOdd(arr[i]))
+                temp[k++] = arr[i];
         }
         for (int i = 0; i < arr.length; i++) {
-            if(!isOdd(arr[i])) temp[k++] = arr[i];
+            if (!isOdd(arr[i]))
+                temp[k++] = arr[i];
         }
         return temp;
-    } 
-    
+    }
+
     boolean Test(int arr[]) {
         for (int i = 1; i < arr.length; i++) {
-            if(!isOdd(arr[i-1]) && isOdd(arr[i])) return false;
+            if (!isOdd(arr[i - 1]) && isOdd(arr[i]))
+                return false;
         }
         return true;
     }
 
     public static void main(String[] args) {
-    
+
         Task2 task = new Task2();
-        int arr[] = {3,4,1,2,6,7};
+        int arr[] = { 3, 4, 1, 2, 6, 7 };
+        System.out.println("Before");
+        System.out.println(Arrays.toString(arr));
         int updated[] = task.Sort(arr);
-        System.out.println(task.Test(updated));
+        System.out.println("After");
         System.out.println(Arrays.toString(updated));
-        
+        System.out.println();
+
+        Random random = new Random();
+        for (int i = 0; i < 5; i++) {
+            int arr2[] = new int[10];
+            for (int j = 0; j < arr2.length; j++) {
+                arr2[j] = random.nextInt(50);
+            }
+
+            System.out.println("Before");
+            System.out.println(Arrays.toString(arr2));
+            updated = task.Sort(arr2);
+            System.out.println("After");
+            System.out.println(Arrays.toString(updated));
+            System.out.println();
+        }
+
     }
 
 }
-
-
